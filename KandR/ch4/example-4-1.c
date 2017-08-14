7fcdb8ab3aa826c2abda8effbd5dc1673d95f714
failbutter <failbutter@163.com>
2017-08-14 14:53:08 +0800

    finished example-4-1,comparing to the one in the book

--- KandR/ch4/example-4-1.c
+++ KandR/ch4/example-4-1.c
@@ -10,32 +10,53 @@
 #include <stdio.h>
 #include <string.h>
 #define maxChar 70
+
+int inLine(char s[])
+{
+  /*sizeof strlen*/
+  for(int i=0;i<=(strlen(s)-4);i++)
+    {
+      if((s[i]=='o')&&(s[i+1]=='u')&&(s[i+2]=='l')&&(s[i+3]=='d'))
+	{return 1;}
+      
+    }
+  return 0;
+}
 int main()
 {
    char inString[] = "Ah Love! could you and I with Fate conspire\nTo grasp this sorry Scheme of Things entire,\nWould not we shatter it to bits -- and then\nRe-mould it nearer to the Heart's Desire!";
-
+   /*position for the end of this line*/
+   /*newPo for the beginning of the next line*/
    int position=0;
+   int newPo=0;
    int lenline=0;
    char line[maxChar];/*initialize array? to a given value?*/
+   /*fundumental conflict: the process of first line is different from other lines*/
    while(inString[position]!='\0')
      {
        
-       for(int i=0;(inString[i+position]!='\n')&& (inString[i+position]!='\0');i++)
+       for(int i=0;(inString[i+newPo]!='\n')&& (inString[i+newPo]!='\0');i++)
 	 {
-	   line[i]=inString[i+position];
+	   line[i]=inString[i+newPo];
 	   lenline=i+1;
 	 }
-       printf("%i",position);
-       printf("%i",lenline);
+       // printf("%i",position);
+       // printf("%i",lenline);
        line[lenline]='\0';
        
-       if (1)
+       if (inLine(line))
 	 {
 	   printf("%s\n",line);
 	   }
-       position=position+lenline+1;
+       position=newPo+lenline;
+       newPo=position+1;
        
      }
+
+   // printf("%i",inLine("wdscfoondoouldoo"));
+   // putchar(inString[position]);
+   // putchar(inString[position-1]);
+   //putchar(inString[position-2]);
    /* printf("%u",sizeof(inString));*/
    /*printf(sizeof(inString));*/
    /*printf*/
